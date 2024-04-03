1fe4a8f9e6f647eef8b9d09c55aa95eea3f29ec6
Khai Nguyen <1192669+khaister@users.noreply.github.com>
2024-04-02 18:09:26 -0700

    Call scheduling service from view

--- api/v1/views.py
+++ api/v1/views.py
@@ -1,11 +1,31 @@
 from rest_framework import permissions, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST
+
+from core.services import scheduling_service
 
 
 class RequestViewSet(viewsets.ViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
-    @action(methods=['get'], detail=False, url_path='next-available', url_name='next-available')
+    @action(
+        methods=["get"],
+        detail=False,
+        url_path="next-available",
+        url_name="next-available",
+    )
     def next_available(self, request):
-        return Response({'message': 'Next available appointment'})
+        latitude = request.query_params.get("lat")
+        longitude = request.query_params.get("lon")
+        if not latitude or not longitude:
+            return Response(
+                {"message": "Missing latitude or longitude"},
+                status=HTTP_400_BAD_REQUEST,
+            )
+
+        appointment = scheduling_service.get_next_available_appointment(
+            latitude, longitude
+        )
+
+        return Response(appointment or {"message": "No appointments available"})
