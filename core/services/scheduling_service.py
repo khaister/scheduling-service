a7321f8d9bd791ac59c84dba8a13049aa350ee0b
Khai Nguyen <1192669+khaister@users.noreply.github.com>
2024-04-02 20:38:29 -0700

    Look for first availability for all locations for a given zip

--- core/services/scheduling_service.py
+++ core/services/scheduling_service.py
@@ -19,14 +19,17 @@ def get_next_available_appointment(latitude, longitude) -> dict | None:
         return None
 
     # translate zip code to location id
-    location_id = location_dal.get_location_id(zip_code)
-    if not location_id:
+    location_ids = location_dal.get_location_ids(zip_code)
+    if not location_ids:
         _logger.info("Location id not found for zip code %s", zip_code)
         return None
 
-    next_available = partner_client.get_next_available(location_id)
+    for location_id in location_ids:
+        next_available = partner_client.get_next_available(location_id)
+        if next_available:
+            return {
+                "location_id": location_id,
+                "appointment_time": next_available.get("epoch_time"),
+            }
 
-    return {
-        "location_id": location_id,
-        "appointment_time": next_available and next_available.get("epoch_time"),
-    }
+    return None
